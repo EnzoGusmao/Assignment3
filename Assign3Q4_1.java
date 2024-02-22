862f15ecb39ca234ee88c517fd13c9692f6297b7
EnzoGusmao <159530740+EnzoGusmao@users.noreply.github.com>
2024-02-22 16:52:09 +0000

    q4

---
+++ Assign3Q4_1.java
@@ -0,0 +1,11 @@
+public class Assign3Q4 {
+    public static void main(String args[]){
+
+    for (int l=0; l<4; l++){
+        for (int n=0; n<10; n++){
+       System.out.print("*");       
+    } 
+    System.out.println();
+    }
+}
+}
---
+++ Assign3Q4_2.java
@@ -0,0 +1,11 @@
+public class Assign3Q4 {
+    public static void main(String args[]){
+        
+    for (int lines=0; lines<=5; lines++){
+        for (int caracter=0; caracter<lines; caracter++){
+       System.out.print("*");       
+    } 
+    System.out.println();
+    }
+}
+}
