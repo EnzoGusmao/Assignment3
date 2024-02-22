a8ce28faa1d9f80c897de77d3c11a8640a84c17b
EnzoGusmao <159530740+EnzoGusmao@users.noreply.github.com>
2024-02-22 21:16:48 +0000

    q6

---
+++ Assign3Q6.java
@@ -0,0 +1,14 @@
+public class Assign3Q6 {
+    public static void main(String args[]){
+        int l = 0;
+        while (l < 4) {
+            int n = 0;
+            while (n < 10) {
+                System.out.print("*");
+                n++;
+            }
+            System.out.println();
+            l++;
+        }
+    }
+}
